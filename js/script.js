21060783195a2d883a0b7b1a578ccededa64709a
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-20 07:34:11 +0000

    made a discount checker

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function checkDiscount() {
+  const ageInput = parseInt(document.getElementById("age-entered").value)
+  const dayOfWeek = document.getElementById("day-of-week").value
+  let discountMessage = ""
+
+  if ((dayOfWeek === "Wednesday" && ageInput >= 60) || (dayOfWeek === "Sunday" && ageInput <= 12)) {
+    discountMessage = "You get a discount!"
+  } else {
+    discountMessage = "No discount for you."
+  }
+
+  document.getElementById("answer").innerHTML = discountMessage
+}
